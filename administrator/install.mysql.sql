2358f783f6f55938577c6cca2091187a7d46103a
Danny Bouman <dannybb@gmail.com>
2012-07-06 12:00:49 -0400

    Initial commit

---
+++ administrator/install.mysql.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS `#__summer_reader` (
+`id` int(11) unsigned NOT NULL auto_increment,
+`catid` int(11) unsigned NOT NULL default '0',
+`sid` int(11) unsigned NOT NULL default '0',
+`title` varchar(255) NOT NULL default '',
+`alias` varchar(255) NOT NULL default '',
+`testfield1` varchar(255) NOT NULL default '',
+`testfield2` varchar(255) NOT NULL default '',
+`created` datetime NOT NULL default '0000-00-00 00:00:00',
+`created_by` int(11) unsigned NOT NULL default '0',
+`created_by_alias` varchar(255) NOT NULL default '',
+`modified` datetime NOT NULL default '0000-00-00 00:00:00',
+`modified_by` int(11) unsigned NOT NULL default '0',
+`checked_out` int(11) unsigned NOT NULL default '0',
+`checked_out_time` datetime NOT NULL default '0000-00-00 00:00:00',
+`published` tinyint(1)  NOT NULL default '0',
+`ordering` int(11) NOT NULL default '0',
+`params` text NOT NULL,
+`hits` int(11) unsigned NOT NULL default '0',
+PRIMARY KEY (`id`)
+);
